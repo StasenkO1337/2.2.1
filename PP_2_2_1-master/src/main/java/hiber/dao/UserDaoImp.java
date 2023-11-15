e77d04639196a8369de51aed04c1786aa4bd5e82
StasenkO1337 <78676050+StasenkO1337@users.noreply.github.com>
2023-11-15 13:51:23 +0500

    Update UserDaoImp.java
    Y.Y

--- PP_2_2_1-master/src/main/java/hiber/dao/UserDaoImp.java
+++ PP_2_2_1-master/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,6 +2,7 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -21,13 +22,11 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public User getUserByCar(String model, int series) {
-      String hql = "from User user where user.car.model = :model and user.car.series = :series";
-      TypedQuery<User> query = sessionFactory
-              .getCurrentSession()
-              .createQuery(hql)
-              .setParameter("model", model)
-              .setParameter("series", series);
-      return query.setMaxResults(1).getSingleResult();
+      Query query  = sessionFactory.getCurrentSession().createQuery("from User user where user.car.model = :model and user.car.series = :series");
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      User user = (User) query.getSingleResult();
+      return user;
    }
 
    @Override
